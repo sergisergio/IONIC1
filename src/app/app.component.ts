bc89de7885222e82be18c5c992eee81e49a0d992
wa81-15 <khemirifatima@gmail.com>
2019-03-08 13:57:57 +0100

    signup, login, logout with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { OptionsPage } from '../pages/options/options';
+import { AuthPage } from '../pages/auth/auth';
+
+import * as firebase from 'firebase';
 
 @Component({
   templateUrl: 'app.html'
@@ -13,6 +16,8 @@ export class MyApp {
 
     tabsPage:any = TabsPage;
     optionsPage:any = OptionsPage;
+    authPage:any = AuthPage;
+    isAuth:boolean;
 
     @ViewChild('content') content: NavController;
 
@@ -23,11 +28,36 @@ export class MyApp {
         platform.ready().then(() => {
             statusBar.styleDefault();
             splashScreen.hide();
+            let config = {
+                apiKey: "AIzaSyBH9_dZS0Rz64HIxOZXVAEgtMdKHITNP7Y",
+                authDomain: "ionic-oc-ba1cc.firebaseapp.com",
+                databaseURL: "https://ionic-oc-ba1cc.firebaseio.com",
+                projectId: "ionic-oc-ba1cc",
+                storageBucket: "ionic-oc-ba1cc.appspot.com",
+                messagingSenderId: "767466858293"
+            };
+            firebase.initializeApp(config);
+            firebase.auth().onAuthStateChanged(
+                (user) => {
+                    if (user) {
+                        this.isAuth = true;
+                        this.content.setRoot(TabsPage);
+                    } else {
+                        this.isAuth = false;
+                        this.content.setRoot(AuthPage, {mode: 'connect'});
+                    }
+                }
+            );
         });
     }
 
-    onNavigate(page: any) {
-        this.content.setRoot(page);
+    onNavigate(page: any, data?: {}) {
+        this.content.setRoot(page, data ? data : null);
+        this.menuCtrl.close();
+    }
+
+    onDisconnect() {
+        firebase.auth().signOut();
         this.menuCtrl.close();
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { SettingsPage } from '../pages/settings/settings';
 import { AppareilFormPage } from '../pages/appareils/appareil-form/appareil-form';
 import { OptionsPage } from '../pages/options/options';
 import { TabsPage } from '../pages/tabs/tabs';
+import { AuthPage } from '../pages/auth/auth';
 import { AppareilsService } from "../services/appareils.service";
+import { AuthService } from "../services/auth.service";
+
 
 @NgModule({
   declarations: [
@@ -23,7 +26,8 @@ import { AppareilsService } from "../services/appareils.service";
     SettingsPage,
     TabsPage,
     OptionsPage,
-    AppareilFormPage
+    AppareilFormPage,
+    AuthPage
   ],
   imports: [
     BrowserModule,
@@ -38,13 +42,15 @@ import { AppareilsService } from "../services/appareils.service";
     SettingsPage,
     TabsPage,
     OptionsPage,
-    AppareilFormPage
+    AppareilFormPage,
+    AuthPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AppareilsService
+    AppareilsService,
+    AuthService
   ]
 })
 export class AppModule {}
